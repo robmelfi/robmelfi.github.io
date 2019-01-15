ab254b489adb610c3d1bdc9ad69af5ef2a319663
robmelfi <melfiroberto@gmail.com>
2019-01-15 19:02:24 +0100

    update source test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <p>robmelfi</p>
+          <p>robmelfi.</p>
         </header>
       </div>
     );
