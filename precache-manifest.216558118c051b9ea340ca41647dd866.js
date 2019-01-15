f6a26ee62136f1fe1a0dd58395319e51069999f9
robmelfi <melfiroberto@gmail.com>
2019-01-15 17:55:18 +0100

    Updates

---
+++ precache-manifest.216558118c051b9ea340ca41647dd866.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f46cd6ef720b1f19b637",
+    "url": "/static/js/main.f46cd6ef.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "f46cd6ef720b1f19b637",
+    "url": "/static/css/main.f4364149.chunk.css"
+  },
+  {
+    "revision": "423dffe2e3904777f707b33165ec6b9b",
+    "url": "/index.html"
+  }
+];
