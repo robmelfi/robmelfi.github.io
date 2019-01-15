a266517aa8321606fda27da0e738621c9c687f20
robmelfi <melfiroberto@gmail.com>
2019-01-15 19:03:01 +0100

    Updates

---
+++ precache-manifest.5df9cdb7143ebfa908bb43be92d9e26a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a9f1211e17d3b3c0381e",
+    "url": "/static/js/main.a9f1211e.chunk.js"
+  },
+  {
+    "revision": "b4a0c8291ae6662d7ee6",
+    "url": "/static/js/1.b4a0c829.chunk.js"
+  },
+  {
+    "revision": "a9f1211e17d3b3c0381e",
+    "url": "/static/css/main.bfac7229.chunk.css"
+  },
+  {
+    "revision": "60dec90f660da740ef9d6360117f9aaf",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fad05906e9b8846bd3d4cfe2e0f52361.js"
+  "/precache-manifest.5df9cdb7143ebfa908bb43be92d9e26a.js"
 );
 
 workbox.clientsClaim();
