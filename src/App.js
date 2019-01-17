5b4faa4eabe948b468c88909a845469ba0319472
robmelfi <melfiroberto@gmail.com>
2019-01-17 14:50:28 +0100

    add google analytics tracking code

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
       resumeData: {}
     };
 
-    ReactGA.initialize('UA-110570651-1');
+    ReactGA.initialize('UA-132692830-1');
     ReactGA.pageview(window.location.pathname);
 
   }
