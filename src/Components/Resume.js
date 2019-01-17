46222ee162f75bcbdd23df8097b678d718380458
robmelfi <melfiroberto@gmail.com>
2019-01-17 16:35:18 +0100

    update resume

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,78 +1,105 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class Resume extends Component {
-  render() {
+    render() {
 
-    if(this.props.data){
-      var skillmessage = this.props.data.skillmessage;
-      var education = this.props.data.education.map(function(education){
-        return <div key={education.school}><h3>{education.school}</h3>
-        <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
-        <p>{education.description}</p></div>
-      })
-      var work = this.props.data.work.map(function(work){
-        return <div key={work.company}><h3>{work.company}</h3>
-            <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
-            <p className="newline">{work.description}</p>
-        </div>
-      })
-      var skills = this.props.data.skills.map(function(skills){
-      var projectImage = 'images/tech/'+skills.image;
-        return <div key={skills.name} className="columns feature-item">
-                  <img className='skill' alt={skills.name} src={projectImage} />
-                  <h5>{skills.name}</h5>
-                  <p>{skills.description}</p>
-               </div>
-      })
+        let skillmessage;
+        let education;
+        let work;
+        let publication;
+        let skills;
+
+        if (this.props.data) {
+            skillmessage = this.props.data.skillmessage;
+            education = this.props.data.education.map(function (education) {
+                return <div key={education.school}><h3>{education.school}</h3>
+                    <p className="info">{education.degree} <span>&bull;</span><em
+                        className="date">{education.graduated}</em></p>
+                    <p>{education.description}</p></div>
+            });
+            work = this.props.data.work.map(function (work) {
+                return <div key={work.company}><h3>{work.company}</h3>
+                    <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
+                    <p className="newline">{work.description}</p>
+                </div>
+            });
+            publication = this.props.data.publication.map(function (publication) {
+                return <div key={publication.title}>
+                            <h5>{publication.title}</h5>
+                            <p className="newline">
+                                {publication.journal} <br />
+                                {publication.authors} <br />
+                                <a href={publication.url}>{publication.title}</a> <br />
+                            </p>
+                       </div>
+            });
+            skills = this.props.data.skills.map(function (skills) {
+                var projectImage = 'images/tech/' + skills.image;
+                return <div key={skills.name} className="columns feature-item">
+                    <img className='skill' alt={skills.name} src={projectImage}/>
+                    <h5>{skills.name}</h5>
+                    <p>{skills.description}</p>
+                </div>
+            });
+        }
+
+        return (
+            <section id="resume">
+
+                <div className="row education">
+                    <div className="three columns header-col">
+                        <h1><span>Education</span></h1>
+                    </div>
+
+                    <div className="nine columns main-col">
+                        <div className="row item">
+                            <div className="twelve columns">
+                                {education}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="row work">
+
+                    <div className="three columns header-col">
+                        <h1><span>Work</span></h1>
+                    </div>
+
+                    <div className="nine columns main-col">
+                        {work}
+                    </div>
+                </div>
+
+                <div className="row publication">
+
+                    <div className="three columns header-col">
+                        <h1><span>Publication</span></h1>
+                    </div>
+
+                    <div className="nine columns main-col">
+                        {publication}
+                    </div>
+                </div>
+
+
+                <div className="row skill">
+
+                    <div className="three columns header-col">
+                        <h1><span>Favorite Tech</span></h1>
+                    </div>
+
+                    <div>
+                        <div className="nine columns main-col"><p className="lead center">{skillmessage}</p></div>
+                        <ul className="bgrid-quarters s-bgrid-thirds cf">
+                            {skills}
+                        </ul>
+                    </div>
+                </div>
+            </section>
+        );
     }
-
-    return (
-      <section id="resume">
-
-      <div className="row education">
-         <div className="three columns header-col">
-            <h1><span>Education</span></h1>
-         </div>
-
-         <div className="nine columns main-col">
-            <div className="row item">
-               <div className="twelve columns">
-                 {education}
-               </div>
-            </div>
-         </div>
-      </div>
-
-
-      <div className="row work">
-
-         <div className="three columns header-col">
-            <h1><span>Work</span></h1>
-         </div>
-
-         <div className="nine columns main-col">
-          {work}
-        </div>
-    </div>
-
-
-
-      <div className="row skill">
-
-         <div className="three columns header-col">
-            <h1><span>Favorite Tech</span></h1>
-         </div>
-
-         <div>
-           <div className="nine columns main-col"><p className="lead center">{skillmessage}</p></div>
-				   <ul className="bgrid-quarters s-bgrid-thirds cf">
-					  {skills}
-					 </ul>
-			  </div>
-      </div>
-   </section>
-    );
-  }
 }
 
 export default Resume;
