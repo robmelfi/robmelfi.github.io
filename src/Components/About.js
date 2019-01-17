c34225f48188c324a693393d88208678d820685e
robmelfi <melfiroberto@gmail.com>
2019-01-17 15:53:53 +0100

    update about

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,54 +1,65 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class About extends Component {
-  render() {
+    render() {
 
-    if(this.props.data){
-      var name = this.props.data.name;
-      var profilepic= "images/"+this.props.data.image;
-      var bio = this.props.data.bio;
-      var city = this.props.data.address.city;
-      var state = this.props.data.address.state;
-      var zip = this.props.data.address.zip;
-      var phone= this.props.data.phone;
-      var email = this.props.data.email;
-      var resumeDownload = this.props.data.resumedownload;
+        let name;
+        let profilepic;
+        let bio;
+        let city;
+        let state;
+        let zip;
+        let phone;
+        let email;
+        let resumeDownload;
+
+        if (this.props.data) {
+            name = this.props.data.name;
+            profilepic = "images/" + this.props.data.image;
+            bio = this.props.data.bio;
+            city = this.props.data.address.city;
+            state = this.props.data.address.state;
+            zip = this.props.data.address.zip;
+            phone = this.props.data.phone;
+            email = this.props.data.email;
+            resumeDownload = this.props.data.resumedownload;
+        }
+
+        return (
+            <section id="about">
+                <div className="row">
+                    <div className="three columns">
+                        <img className="profile-pic" src={profilepic} alt="Tim Baker Profile Pic"/>
+                    </div>
+                    <div className="nine columns main-col">
+                        <h2>About Me</h2>
+
+                        <p>{bio}</p>
+                        <div className="row">
+                            <div className="columns contact-details">
+                                <h2>Contact Details</h2>
+                                <p className="address">
+                                    <span>{name}</span><br/>
+                                    <span>{city}<br/>
+                                          {state} {zip}
+                                    </span><br/>
+                                    <span>{phone}</span><br/>
+                                    <span><a href="mailto:tbakerx@gmail.com">{email}</a></span>
+                                </p>
+                            </div>
+                            <div className="columns download">
+                                <p>
+                                    <a href={resumeDownload} className="button" target="_blank"
+                                       rel="noopener noreferrer"><i className="fa fa-download"></i>Download Resume</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </section>
+        );
     }
-
-    return (
-      <section id="about">
-      <div className="row">
-         <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="Tim Baker Profile Pic" />
-         </div>
-         <div className="nine columns main-col">
-            <h2>About Me</h2>
-
-            <p>{bio}</p>
-            <div className="row">
-               <div className="columns contact-details">
-                  <h2>Contact Details</h2>
-                  <p className="address">
-						   <span>{name}</span><br />
-						   <span>{city}<br />
-						         {state}, {zip}
-                   </span><br />
-						   <span>{phone}</span><br />
-                     <span><a href="mailto:tbakerx@gmail.com">{email}</a></span>
-					   </p>
-               </div>
-               <div className="columns download">
-                  <p>
-                     <a href={resumeDownload} className="button" target="_blank" rel="noopener noreferrer"><i className="fa fa-download"></i>Download Resume</a>
-                  </p>
-               </div>
-            </div>
-         </div>
-      </div>
-
-   </section>
-    );
-  }
 }
 
 export default About;
