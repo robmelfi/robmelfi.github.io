e503f6d3dd4c8df5a5ae8182b501eb6040c26f10
robmelfi <melfiroberto@gmail.com>
2019-01-18 15:10:42 +0100

    add testimonial

--- src/Components/Testimonials.js
+++ src/Components/Testimonials.js
@@ -1,38 +1,44 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class Testimonials extends Component {
-  render() {
+    render() {
 
-    if(this.props.data){
-      var testimonials = this.props.data.testimonials.map(function(testimonials){
-        return  <li key={testimonials.user}>
-            <blockquote>
-               <p>{testimonials.text}</p>
-               <cite>{testimonials.user}</cite>
-            </blockquote>
-         </li>
-      })
+        let testimonials;
+            if (this.props.data) {
+                testimonials = this.props.data.testimonials.map(function (testimonials) {
+                    return <li key={testimonials.user}>
+                        <blockquote>
+                            <p>{testimonials.text}</p>
+                            <cite>
+                                {testimonials.linkedin ? <a href={testimonials.linkedin}>&nbsp;&nbsp;{testimonials.user}</a> : testimonials.user}
+                                {testimonials.linkedin ? <a href={testimonials.linkedin}>&nbsp;&nbsp;<i className="fa fa-linkedin" /></a> : null}
+                            </cite>
+                        </blockquote>
+                    </li>
+                })
+            }
+
+            return (
+                <section id="testimonials">
+                    <div className="text-container">
+                        <div className="row">
+
+                            <div className="two columns header-col">
+                                <h1><span>Client Testimonials</span></h1>
+                            </div>
+
+                            <div className="ten columns flex-container">
+                                <ul className="slides">
+                                    {testimonials}
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            );
+        }
     }
 
-    return (
-      <section id="testimonials">
-      <div className="text-container">
-         <div className="row">
-
-            <div className="two columns header-col">
-               <h1><span>Client Testimonials</span></h1>
-            </div>
-
-            <div className="ten columns flex-container">
-                  <ul className="slides">
-                      {testimonials}
-                  </ul>
-               </div>
-            </div>
-         </div>
-   </section>
-    );
-  }
-}
-
-export default Testimonials;
+    export
+    default
+    Testimonials;
